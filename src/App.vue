c4ca442cb5a42e88c78b382d63beaba0216ffbbd
SkyfallWasTaken <mahadkalam1@proton.me>
2024-07-08 20:07:33 +0100

    addons, get data

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <script setup lang="ts">
 import "@xterm/xterm/css/xterm.css";
 import "./style.css";
+import { onMounted, ref } from "vue";
 
 import { Terminal } from "@xterm/xterm";
-import { onMounted, ref } from "vue";
+import { ClipboardAddon } from "@xterm/addon-clipboard";
+import { WebLinksAddon } from "@xterm/addon-web-links";
+import { FitAddon } from "@xterm/addon-fit";
+
+import * as KeyCode from "keycode-js";
 
 const terminalElement = ref<HTMLElement>();
 
 onMounted(() => {
   const term = new Terminal();
+  const fitAddon = new FitAddon();
+  const addons = [fitAddon, new ClipboardAddon(), new WebLinksAddon()];
+  addons.forEach((addon) => {
+    term.loadAddon(addon);
+  });
+  console.debug(`Loaded ${addons.length} addons`);
+
   term.open(terminalElement.value!);
+
+  fitAddon.fit();
   term.write("Hello from Phantom! $ ");
+
+  term.onData((data) => {
+    if (data.charCodeAt(data.length - 1) === KeyCode.KEY_RETURN) {
+      alert("Enter");
+    } else {
+      term.write(data);
+    }
+  });
 });
 </script>
 
