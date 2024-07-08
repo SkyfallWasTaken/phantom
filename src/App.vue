5e6818fd127715d28454754dfa08d662832bc127
SkyfallWasTaken <mahadkalam1@proton.me>
2024-07-08 19:50:25 +0100

    Big bang

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import "@xterm/xterm/css/xterm.css";
+import "./style.css";
+
+import { Terminal } from "@xterm/xterm";
+import { onMounted, ref } from "vue";
+
+const terminalElement = ref<HTMLElement>();
+
+onMounted(() => {
+  const term = new Terminal();
+  term.open(terminalElement.value!);
+  term.write("Hello from Phantom! $ ");
+});
+</script>
+
+<template>
+  <div>
+    <div ref="terminalElement" />
+  </div>
+</template>
