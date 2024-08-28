16bdd7cea78cb4a21722ceb58fe3a3f85313749f
SkyfallWasTaken <mahadkalam1@proton.me>
2024-08-28 13:45:19 +0100

    Fix backspace

--- src/phantom/main.ts
+++ src/phantom/main.ts
@@ -67,6 +67,11 @@ export class Phantom {
         }
 
         this.terminal.write("\r\n" + this.prompt);
+      } else if (lastKey === KeyCode.KEY_F16) {
+        if (this.prompt.length + this.command.length !== this.prompt.length) {
+          this.command = this.command.slice(0, -1);
+          this.terminal.write("\x1b[D\x1b[P"); // Moves cursor back one space and deletes the character
+        }
       } else if (lastKey === 65 && data === "\x1b[A") {
         // Up arrow
         if (this.historyCursor > 0) {
